22cf195d3465ab14f71d3061d86612d81f14f4c2
Eduardo Levi <eduardolevi@MacBook-de-Eduardo.local>
2020-04-22 12:04:02 -0700

    update menu responsivo

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ var presente = document.getElementById("btPresente");
 var lista = document.getElementById("btLista");
 var doacao = document.getElementById("btDoacao");
 
+const abreMenu = document.querySelector('.abreMenu');
+const menu = document.querySelector('nav > ul');
+
 var btns = [inicio, participar, presente, lista, doacao ];
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
@@ -11,5 +14,22 @@ for (var i = 0; i < btns.length; i++) {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace("active", "");
     this.className += " active";
+    menu.classList.remove('collapse');
   });
 }
+
+
+abreMenu.addEventListener('click', function () {
+  let abre = menu.classList.contains('collapse');
+  if(abre) {
+    menu.classList.remove('collapse');
+  } else {
+    menu.classList.add('collapse');    
+  }
+})
+
+// btns.addEventListener('click', function() {
+//   if(abre) {
+//     menu.classList.remove('collapse');
+//   }
+// })
